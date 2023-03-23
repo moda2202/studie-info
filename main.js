a47289be101bce002d8265c57a13b79147060518
mounir Darwich <moda2202@student.miun.se>
2023-03-24 00:08:07 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+document.getElementById("math").addEventListener("click", function() {
+    window.location.href = "math.html";
+  });
+  
+  document.getElementById("database").addEventListener("click", function() {
+    window.location.href = "database.html";
+  });
+  
+  document.getElementById("mjukvaru").addEventListener("click", function() {
+    window.location.href = "mukvaru.html";
+  });
+    document.getElementById("OOP_JAVA").addEventListener("click", function() {
+      window.location.href = "OOP.html";
+  });
